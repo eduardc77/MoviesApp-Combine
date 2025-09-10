1b71f2e4278d7ac23a7c52931a119b36e0f033e5
Eduardc77 <36419167+eduardc77@users.noreply.github.com>
2025-09-10 11:57:42 +0300

    Add files via upload

---
+++ Movies/MoviesTests/MoviesTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by User on 9/10/25.
+//
+
+import Testing
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
